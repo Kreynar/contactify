7d8ec86cb744d77e0552014579389568ddbd5627
Kreynar <martynasgs@gmail.com>
2019-08-25 17:13:45 +0300

    finished footer

--- src/App/Footer/Footer.js
+++ src/App/Footer/Footer.js
@@ -19,26 +19,30 @@ const Footer = () =>
   </div>
   <div className='middle-container'>
     <div className='middle-inner-container'>
-      <div><i class="fas fa-cloud-upload-alt"></i></div>
-      <div>
+      <div className='center big-icon'><i class="fas fa-cloud-upload-alt"></i></div>
+      <div className='center last-synced-container'>
         <div>Last synced:</div>
         <div>{MOCK_LAST_SYNC_DATE}</div>
       </div>
-      <div>
+      <div className='center force-sync-line'>
         <i class="fas fa-sync-alt"></i>
         <a href='#'>Force sync</a>
       </div>
     </div>
     <div className='middle-inner-container'>
-      <div><i class="fas fa-stethoscope"></i></div>
-      <div>
+      <div className='center big-icon'><i class="fas fa-stethoscope"></i></div>
+      <div className='help-desk-container'>
         <div>Help desk and Resolution center available:</div>
         <div>{HELP_DESK_AVAILABLE_TIME}</div>
       </div>
-      <div></div>
-      <div></div>
     </div>
   </div>
+  <div className='right-container'>
+      <div><a href='#'>Groups</a></div>
+      <div><a href='#'>Frequently contacted</a></div>
+      <div><a href='#'>Preferences</a></div>
+      <div><a href='#'>Log out</a></div>
+    </div>
 </div>
 
 export default Footer;

f3d638e472c9680b3057566a9a8bfe6268c53328
Kreynar <martynasgs@gmail.com>
2019-08-27 01:18:02 +0300

    added handling case, when all users are filtered out

--- src/App/Contacts/Contacts.js
+++ src/App/Contacts/Contacts.js
@@ -21,9 +21,6 @@ const Contacts = () => {
   const [cityFilter, setCityFilter] = useState('');
   const [activeFilter, setActiveFilter] = useState(false);
   const [selectedContactId, setSelectedContactId] = useState(contacts[0].id);
-  const selectedContact = contacts.find(
-    contact => contact.id === selectedContactId
-  );
   const setFilters = ({
     name,
     city,
@@ -39,22 +36,29 @@ const Contacts = () => {
     cityFilter,
     activeFilter
   });
+  const selectedContact = filteredContacts.find(
+    contact => contact.id === selectedContactId
+  );
   return (
     <div>
       <FilterBar onFilterClick={setFilters}/>
       <div className='users-view-container'>
-        <div className='card-container'>
-          <UserCard name={selectedContact.name}
-                    surname={selectedContact.surname}
-                    city={selectedContact.city}
-                    phone={selectedContact.phone}
-                    email={selectedContact.email}/>
-        </div>
-        <div className='table-container'>
-          <UsersTable contacts={filteredContacts}
-                      selectedContactId={selectedContactId}
-                      onContactSelect={setSelectedContactId}/>
-        </div>
+        {filteredContacts.length
+        ? <>
+          <div className='card-container'>
+            <UserCard name={selectedContact.name}
+                      surname={selectedContact.surname}
+                      city={selectedContact.city}
+                      phone={selectedContact.phone}
+                      email={selectedContact.email}/>
+          </div>
+          <div className='table-container'>
+            <UsersTable contacts={filteredContacts}
+                        selectedContactId={selectedContactId}
+                        onContactSelect={setSelectedContactId}/>
+          </div>
+        </>
+        : <div className='no-users-found'>No users found</div>}
       </div>
     </div>
   );
